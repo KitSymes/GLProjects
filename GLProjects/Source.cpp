90df318a5ff024e280c3431fc0d4e329d9187425
KitSymes <kitsymes@btinternet.com>
2021-02-02 18:06:51 +0000

    #003
    Camera + 3D

--- GLProjects/Source.cpp
+++ GLProjects/Source.cpp
@@ -7,27 +7,60 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
+Vertex Source::vertices[] = { 1, 1, 1, -1, 1, 1, -1, -1, 1, -1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, -1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, -1, -1, 1, -1, -1, 1, -1, 1, 1, -1, 1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, 1, -1, -1, 1, -1, 1, 1, -1, 1, -1, -1 };
+Colour Source::colours[] = { 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1 };
+
+Vertex Source::indexedVertices[] = { 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, -1, -1, -1, -1 };
+Colour Source::indexedColors[] = { 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0 };
+GLushort Source::indices[] = {0, 1, 2, 2, 3, 0, 0, 3, 4, 4, 5, 0, 0, 5, 6, 6, 1, 0, 1, 6, 7, 7, 2, 1, 7, 4, 3, 3, 2, 7, 4, 7, 6, 6, 5, 4};
+
 Source::Source(int argc, char* argv[])
 {
 	rotation = 0.0f;
+	camera = new Camera();
+	camera->eye.x = 5.0f;
+	camera->eye.y = 5.0f;
+	camera->eye.z = -5.0f;
+	camera->center.x = 0.0f;
+	camera->center.y = 0.0f;
+	camera->center.z = 0.0f;
+	camera->up.x = 0.0f;
+	camera->up.y = 1.0f;
+	camera->up.z = 0.0f;
 
 	GLUTCallbacks::Init(this);
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE);
+	glutInitWindowSize(800, 800);
 	glutCreateWindow("GL Project");
 	glutDisplayFunc(GLUTCallbacks::Display);
 	glutTimerFunc(REFRESH_RATE, GLUTCallbacks::Timer, REFRESH_RATE);
 	glutKeyboardFunc(GLUTCallbacks::Keyboard);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	// Set the viewport to encase the entire window
+	glViewport(0, 0, 800, 800);
+	// Set the correct perspective
+	// FoV, Aspect Ratio, Near Clipping, Far Clipping
+	gluPerspective(45, 1, 0, 1000);
+	glMatrixMode(GL_MODELVIEW);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
 	glutMainLoop();
 }
 
 Source::~Source(void)
 {
-
+	delete camera;
 }
 
 void Source::Update()
 {
+	glLoadIdentity();
+	gluLookAt(camera->eye.x, camera->eye.y, camera->eye.z,
+		camera->center.x, camera->center.y, camera->center.z,
+		camera->up.x, camera->up.y, camera->up.z);
+
 	if (rotation >= 360.0f)
 		rotation = 0.0f;
 
@@ -38,35 +71,44 @@ void Source::Display()
 {
 	glClear(GL_COLOR_BUFFER_BIT); // Clears frame
 
-	glPushMatrix();
-	glTranslatef(-0.5f, 0.5f, 0.0f);
+	//DrawCube();
+	//DrawCubeArray();
+	DrawIndexedCube();
+
+	/*glPushMatrix();
+	glTranslatef(-0.5f, 0.5f, -2.0f);
 	glRotatef(rotation, 0.0f, 0.0f, -1.0f);
 	glColor4f(1.0f, 1.0f, 1.0f, 0.0f);
 	DrawHex(5);
 	glPopMatrix();
 
 	glPushMatrix();
-	glTranslatef(-0.5f, 0.5f, 0.0f);
+	glTranslatef(-0.5f, 0.5f, -2.0f);
 	glRotatef(-rotation, 0.0f, 0.0f, -1.0f);
 	glColor4f(0.0f, 0.0f, 0.0f, 0.0f);
 	DrawHex(2.5);
 	glPopMatrix();
 
 	glPushMatrix();
-	glTranslatef(0.5, 0.5, 0);
+	glTranslatef(0.5f, 0.5f, -2.0f);
 	glRotatef(-rotation, 0.0f, 0.0f, -1.0f);
 	glColor4f(1.0f, 1.0f, 1.0f, 0.0f);
 	DrawHex(5);
 	glPopMatrix();
 
 	glPushMatrix();
-	glTranslatef(0.5f, 0.5f, 0.0f);
+	glTranslatef(0.5f, 0.5f, -2.0f);
 	glRotatef(rotation, 0.0f, 0.0f, -1.0f);
 	glColor4f(0.0f, 0.0f, 0.0f, 0.0f);
 	DrawHex(2.5);
 	glPopMatrix();
 
+	glPushMatrix();
+	glTranslatef(0.0f, 0.0f, -2.0f);
+	glRotatef(rotation, 1.0f, 0.0f, 0.0f);
+	glColor4f(0.0f, 0.0f, 0.0f, 0.0f);
 	DrawPolygon();
+	glPopMatrix();*/
 
 	glFlush(); // Sends to Graphics Card or something
 	glutSwapBuffers();
@@ -119,10 +161,148 @@ void Source::DrawHex(float scale)
 	glEnd();
 }
 
+void Source::DrawCube()
+{
+	glBegin(GL_TRIANGLES);
+	{
+		glColor3f(1, 1, 1);
+		glVertex3f(1, 1, 1);
+		glColor3f(1, 1, 0);
+		glVertex3f(-1, 1, 1);
+		glColor3f(1, 0, 0);
+		glVertex3f(-1, -1, 1);
+
+		glColor3f(1, 0, 0);
+		glVertex3f(-1, -1, 1);
+		glColor3f(1, 0, 1);
+		glVertex3f(1, -1, 1);
+		glColor3f(1, 1, 1);
+		glVertex3f(1, 1, 1);
+	}
+
+	{
+		glColor3f(1, 1, 1);
+		glVertex3f(1, 1, 1);
+		glColor3f(1, 0, 1);
+		glVertex3f(1, -1, 1);
+		glColor3f(0, 0, 1);
+		glVertex3f(1, -1, -1);
+
+		glColor3f(0, 0, 1);
+		glVertex3f(1, -1, -1);
+		glColor3f(0, 1, 1);
+		glVertex3f(1, 1, -1);
+		glColor3f(1, 1, 1);
+		glVertex3f(1, 1, 1);
+	}
+
+	{
+		glColor3f(1, 1, 1);
+		glVertex3f(1, 1, 1);
+		glColor3f(0, 1, 1);
+		glVertex3f(1, 1, -1);
+		glColor3f(0, 1, 0);
+		glVertex3f(-1, 1, -1);
+
+		glColor3f(0, 1, 0);
+		glVertex3f(-1, 1, -1);
+		glColor3f(1, 1, 0);
+		glVertex3f(-1, 1, 1);
+		glColor3f(1, 1, 1);
+		glVertex3f(1, 1, 1);
+	}
+
+	{
+		glColor3f(1, 1, 0);
+		glVertex3f(-1, 1, 1);
+		glColor3f(0, 1, 0);
+		glVertex3f(-1, 1, -1);
+		glColor3f(0, 0, 0);
+		glVertex3f(-1, -1, -1);
+
+		glColor3f(0, 0, 0);
+		glVertex3f(-1, -1, -1);
+		glColor3f(1, 0, 0);
+		glVertex3f(-1, -1, 1);
+		glColor3f(1, 1, 0);
+		glVertex3f(-1, 1, 1);
+	}
+
+	{
+		glColor3f(0, 0, 0);
+		glVertex3f(-1, -1, -1);
+		glColor3f(0, 0, 1);
+		glVertex3f(1, -1, -1);
+		glColor3f(1, 0, 1);
+		glVertex3f(1, -1, 1);
+
+		glColor3f(1, 0, 1);
+		glVertex3f(1, -1, 1);
+		glColor3f(1, 0, 0);
+		glVertex3f(-1, -1, 1);
+		glColor3f(0, 0, 0);
+		glVertex3f(-1, -1, -1);
+	}
+
+	{
+		glColor3f(0, 0, 1);
+		glVertex3f(1, -1, -1);
+		glColor3f(0, 0, 0);
+		glVertex3f(-1, -1, -1);
+		glColor3f(0, 1, 0);
+		glVertex3f(-1, 1, -1);
+
+		glColor3f(0, 1, 0);
+		glVertex3f(-1, 1, -1);
+		glColor3f(0, 1, 1);
+		glVertex3f(1, 1, -1);
+		glColor3f(0, 0, 1);
+		glVertex3f(1, -1, -1);
+	}
+
+
+	glEnd();
+}
+
+void Source::DrawCubeArray()
+{
+	glPushMatrix();
+	glBegin(GL_TRIANGLES);
+	for (int i = 0; i < 36; i++)
+	{
+		glColor3fv(&colours[i].r);
+		glVertex3fv(&vertices[i].x);
+	}
+	glEnd();
+	glPopMatrix();
+}
+
+void Source::DrawIndexedCube()
+{
+	glPushMatrix();
+	glBegin(GL_TRIANGLES);
+	for (int i = 0; i < 36; i++)
+	{
+		glColor3fv(&indexedColors[indices[i]].r);
+		glVertex3fv(&indexedVertices[indices[i]].x);
+	}
+	glEnd();
+	glPopMatrix();
+}
+
 void Source::Keyboard(unsigned char key, int x, int y)
 {
-	if (key == 'd')
+	if (key == 'q')
 		rotation += 2.0f;
-	else if (key == 'a')
+	else if (key == 'e')
 		rotation -= 2.0f;
+
+	if (key == 'w')
+		camera->eye.x += 0.1;
+	if (key == 's')
+		camera->eye.x -= 0.1;
+	if (key == 'a')
+		camera->eye.z += 0.1;
+	if (key == 'd')
+		camera->eye.z -= 0.1;
 }
--- GLProjects/Source.h
+++ GLProjects/Source.h
@@ -5,10 +5,37 @@
 #include"GL\freeglut.h"
 #include"GLUTCallbacks.h"
 #define REFRESH_RATE 16
+
+struct Vector3
+{
+	float x, y, z;
+};
+
+struct Camera
+{
+	Vector3 eye, center, up;
+};
+
+struct Colour
+{
+	GLfloat r, g, b;
+};
+
+struct Vertex
+{
+	GLfloat x, y, z;
+};
+
 class Source
 {
 private:
 	float rotation;
+	Camera* camera;
+	static Vertex vertices[];
+	static Colour colours[];
+	static Vertex indexedVertices[];
+	static Colour indexedColors[];
+	static GLushort indices[];
 public:
 	// Constructor
 	Source(int argc, char* argv[]);
@@ -24,6 +51,9 @@ public:
 	// Draw a Regular Hexagon at 0,0 with specified scale
 	void DrawHex(float scale);
 
+	void DrawCube();
+	void DrawCubeArray();
+	void DrawIndexedCube();
 
 	void Keyboard(unsigned char key, int x, int y);
 };
